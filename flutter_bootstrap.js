9d50cd8c033f9e9d59cc53e1e9e1fb7703373176
Sameer Swankar <sameerswankar96@gmail.com>
2025-03-24 22:21:49 +0530

    background bug solved

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3322581713"
+    serviceWorkerVersion: "1233371102"
   }
 });
